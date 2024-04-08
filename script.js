ab275f355676d1985a8b43c40ed8e24371fde291
umarNurmuxamedov2013 <nurmuxamedovumar@gmail.com>
2024-04-08 11:17:02 +0500

    firstcommit

---
+++ script.js
@@ -0,0 +1,46 @@
+let num1 = 5;
+let num2 = 10;
+
+num1 += num2;
+
+console.log(num1);
+
+
+let a = '12';
+let numberA = Number(a);
+
+
+console.log(`Number`);
+
+let random = Math.floor(Math.random() * 100)
+
+if (random % 2 == 0) {
+    console.log(`Random orqali chiqqan son bu ${random} va u juft son `);
+} else {
+    console.log(`Random orqali chiqqan son bu ${random} va u toq son `);
+}
+
+
+let prm = prompt(`so'z kiriting`)
+let prmto = prm.toLowerCase();
+if (prmto == "mars") {
+    console.log(prmto);
+} else {
+    console.log(`Mars It School`)
+}
+
+
+const arr0 = [`apple`, `banana`, `onion`, `watermelon`, `Umar`]
+let split = arr0.split(" ");
+let split2 = split.length;
+const arr1 = []
+for (let i = 0; i < array.length; i++) {
+    if (split2 >= 5) {
+        let push = arr1.push;
+        console.log(arr1);
+    } else {
+
+    }
+
+}
+
